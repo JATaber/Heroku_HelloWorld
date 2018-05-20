798904923041e45b5e16886e70c4d4bb854f124e
XALBIA-PC\james <JATaber@fullsail.edu>
2018-05-20 16:27:45 -0500

    restructured
    Had to do a restruce so that the server would spin up properly

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,7 @@
-module.exports = (express) => {
-  const router = express.Router();
+const router = require('express').Router()
 
-  router.get('/', (req, res) => {
-    res.send('Hello from Heroku!');
-  });
+router.get('/', (req, res) => {
+  res.send('Hello from Heroku!!');
+})
 
-  router.post('/echo', (req, res) => {
-    console.log('Hello from inside Heroku!')
-  });
-
-  return router;
-};
+module.exports = router;
--- src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const mongoose = require('mongoose')
-const app = require('./routes/index.js')
-
-app.set('port', process.env.PORT || 3000);
-
-const server = app.listen(app.get('port'), () => {
-  console.log(`Running on port ${server.address().port}`)
-})
